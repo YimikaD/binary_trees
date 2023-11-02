81eb19c14e3902e37dc336a981a9c3dbc9e88ad4
YimikaD <babsyinny@gmail.com>
2023-11-02 06:49:19 -0700

    Fixed

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the leaves in the binary
+ * @tree: Pointer to the root node of he tree to count the number of leaves
+ * Return: 0 if tree is NULL, numbeer of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_size(tree->left) +
+			binary_tree_size(tree->right) + 1);
+}
